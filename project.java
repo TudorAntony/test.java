555d17dbf6d0d99f6f87a8d780a2fc8b1ab2289c
Tudor Antony <tudorantony6867@gmail.com>
2026-06-06 10:28:02 +0530

    Create project.java

---
+++ project.java
@@ -0,0 +1,8 @@
+public class Addition{
+  public static void main(String args[]){
+    int a = 50;
+    int b = 50;
+    int c = a+b;
+    System.out.println("Addition of two numbers"+c)
+      }
+}
